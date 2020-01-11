9bc893191704c950a4120535bb07026df8da5b11
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-10 19:39:36 -0500

    added user profile page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render() {
-	return (
-		<div>
-		<img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-		<h1>Bank of React</h1>
-
-		<AccountBalance accountBalance={this.props.accountBalance}/>
-		</div>
-        );
+      return(<div>
+      		  <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+      		  <h1>Bank of React</h1>
+            <Link to="/UserProfile">User Profile</Link>
+      		  <AccountBalance accountBalance={this.props.accountBalance}/>
+      		</div>);
     }
 }
 
