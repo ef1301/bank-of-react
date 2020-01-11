5e3985f955c16b82702ed06b6d5ad65855c429c0
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-10 20:13:50 -0500

    implemented getting debits and credits from endpoints

--- src/App.js
+++ src/App.js
@@ -4,27 +4,48 @@ import Home from './components/Home';
 import UserProfile from './components/UserProfile'
 import LogIn from './components/LogIn'
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
   constructor() {
   	super();
   	this.state = {
-  	    accountBalance: 14568.27,
         currentUser: {
           userName: 'bob_loblaw',
           memberSince: '08/23/99',
-        }
+          credit:0,
+          debit:0
+        },
+        debits:[],
+        credits:[]
   	}
   }
-
   mockLogIn = (logInInfo) => {
     const newUser = {...this.state.currentUser}
     newUser.userName = logInInfo.userName
     this.setState({currentUser: newUser})
   }
-
+  accountBalance = () => {
+    let creditTotal = 0
+    let debitTotal = 0
+    for(let i = 0; i < this.state.credits.length; i++){
+      creditTotal += this.state.credits[i].amount
+    }
+    for(let i = 0; i < this.state.debits.length; i++){
+      debitTotal += this.state.debits[i].amount
+    }
+    return creditTotal - debitTotal
+  }
+  async componentDidMount(){
+    const debitEndpoint = 'https://moj-api.herokuapp.com/debits'
+    const creditEndpoint = 'https://moj-api.herokuapp.com/credits'
+    const debits = await axios.get(debitEndpoint)
+    const credits = await axios.get(creditEndpoint)
+    this.setState({debits:debits.data,credits:credits.data})
+    console.log(this.state.debits)
+  }
   render() {
-	  const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+	  const HomeComponent = () => (<Home accountBalance={this.accountBalance()}/>);
     const UserProfileComponent = () => (
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render() {
+      console.log(this.props.accountBalance)
       return(<div>
       		  <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
       		  <h1>Bank of React</h1>
