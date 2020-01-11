7ba509b22b7f323f4ab6ff9fb8a13ebfb351e585
Emily Fang <fangemily60@gmail.com>
2020-01-10 19:07:16 -0500

    UserProfile.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+	super();
+	this.state = {
+	    accountBalance: 14568.27
+	}
+    }
+    render() {
+	
+	const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+	
+	return (
+		<Router>
+		<div>
+		<Route exact path="/" render={HomeComponent}/>
+		</div>
+		</Router>
+	);
+    }
 }
 
 export default App;
